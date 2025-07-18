2cd1f25b54e89cd623cc651b491ff69e0e308799
Akrhiin <akrhiin@gmail.com>
2025-07-18 16:26:43 -0400

    Basic malloc implementation, define basic header structure for memory blocks

--- memory_allocator/main.c
+++ memory_allocator/main.c
@@ -3,14 +3,53 @@
 // created: 07.17.2025
 // updated: 07.17.2025
 
-// Using sbrk because requirements aren't too steep, and implementation is simple.
-//
-// sbrk(0) => gives current address of program break
-// sbrk(x) =(+)=> increments brk by x bytes (allocate memory)
-// sbrk(-x) =(-)=> decrements brk by x bytes (release memory)
+// This code is based off an excellent blog post [https://arjunsreedharan.org/post/148675821737/memory-allocators-101-write-a-simple-memory]. I considered following their example and using sbrk, but decided on mmap for the sake of modernity.
+
+#include <sys/mman.h>
+#include <pthread.h>
+
+#include <stdio.h>
+
+// ----------------------------------------------
+
+// +--------+------------+      +--------+------------+
+// | header | mem block  | =*=> | header | mem block  |
+// +--------+------------+      +--------+------------+
+
+typedef char ALIGN[16]; // Define "ALIGN" as buffer w/ length 16 bytes.
+union header {
+    struct header_t
+    {
+        size_t size;
+        unsigned is_free;
+        struct header_t *next;
+    } s;
+    ALIGN stub; // Make sure header ends up on memory address aligned to 16 bytes.
+                 // ^^ (union sets size based on largest child)
+};
+typedef union header header_t; // Define block header from union definition.
+
+header_t *head, *tail; // Create head and tail pointer for list of blocks.
+
+pthread_mutex_t global_malloc_lock; // Global lock must be acquired before any action on memory.
+                                    //  | On item action => acquire lock
+                                    //  | On action completion => release lock
+
+// ----------------------------------------------
 
 // MALLOC
 void *malloc(size_t size)
 {
+    void *block = mmap(NULL,size,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_PRIVATE,-1,0);
+      // ^^^^ anonymous mapping, so file descriptor set to -1 and offset to 0
+    return block; // if creation of memory block failed, will return NULL pointer
+}
 
+// CALLOC
+// REALLOC
+// FREE
+
+int main()
+{
+    printf("test\n");
 }
